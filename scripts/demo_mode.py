085451bc3903e2a22013f900687050317f691abd
Ben Warzeski <bwarzeski@rethinkrobotics.com>
2014-04-28 13:48:35 -0400

    fix subscriber for estop check

--- scripts/demo_mode.py
+++ scripts/demo_mode.py
@@ -35,6 +35,7 @@ import rospy
 import rospkg
 from baxter_demo_ui import BrrUi
 from baxter_demo_ui.demo_functions import check_calib
+
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Main loop for the demo mode module.
 # Generates UI elements in the following order:
@@ -45,6 +46,8 @@ from baxter_demo_ui.demo_functions import check_calib
 #     Finally, the window will be instantiated with a list of buttons
 # After all windows are instantiated, the BrrUi class will be instantiated.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+
+
 def main():
     rospy.init_node('rsdk_demo_ui')
     rp = rospkg.RosPack()
@@ -52,7 +55,7 @@ def main():
     conf_path = '%s/config.json' % pack_path
 
     commands = ['baxter_interface', 'record', 'playback', 'puppet',
-                'tare', 'calibrate']
+                'tare', 'calibrate', 'joint_trajectory']
     ui = BrrUi(pack_path, conf_path, commands)
     check_calib(ui)
 
